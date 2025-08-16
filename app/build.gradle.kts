b7c7fcdee73b07a537eb8ab60d481239f108f235
Michael Lamykin <michaellamykin2011@gmail.com>
2025-08-16 13:19:19 +0300

    версия 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.newcalculator"
         minSdk = 21
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
